04c09efb69ac65e380f0438670527f4d8147655d
TitovVladimir <chazzii1337@gmail.com>
2017-09-15 17:53:32 +0300

    fix .js

--- lw1/js/search-rnd-array-element.js
+++ lw1/js/search-rnd-array-element.js
@@ -6,24 +6,24 @@ var z=1;
 var a = [];
 var output = "";
 
-  N=10;
+N=10;
 
+var k = prompt("Число для поиска: ");
   
-  var k = prompt("Число для поиска: ");
-  
-  for(i = 1; i <= N; i++){
-	  a[i] = Math.floor(Math.random() * (16 - 1)) + 1;
-	  output = output + "Сгенерированное число[" + i + "из" + N + "]=" + a[i] + "\n";
-	  if (a[i] == k){
-		  z = 2;
-	  }	
-  }
-  if (z == 2)  {
-	 output = output + "Число " + k + " найдено!";
-  } else {
-	 output = output + "Число " + k + " не найдено!";
-	}
-  	  alert(output);
+for(i = 1; i <= N; i++) {
+  a[i] = Math.floor(Math.random() * (16 - 1)) + 1;
+  output = output + "Сгенерированное число[" + i + "из" + N + "]=" + a[i] + "\n";
+    if (a[i] == k) {
+      z = 2;
+	}	
+}
+
+if (z == 2) {
+  output = output + "Число " + k + " найдено!";
+} else {
+  output = output + "Число " + k + " не найдено!";
+}
+alert(output);
 	  
   
   
