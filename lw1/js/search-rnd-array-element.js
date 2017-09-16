9bb7b97da35962b4e0e391ee517cdd6d054bdf33
TitovVladimir <31964744+TitovVladimir@users.noreply.github.com>
2017-09-16 13:14:54 +0300

    Update search-rnd-array-element.js

--- lw1/js/search-rnd-array-element.js
+++ lw1/js/search-rnd-array-element.js
@@ -2,12 +2,12 @@
 var searchNumber;
 var i;
 var isFound = false;
-var randomNumber = [];
+var randomNumbers = [];
 var output = "";
 
 for(i = 1; i <= 10; i++) {
-  randomNumber[i] = Math.floor(Math.random() * (16 - 1)) + 1;
-  output = output + "Сгенерированное число[" + i + "из" + 10 + "]=" + randomNumber[i] + "\n";
+  randomNumbers[i] = Math.floor(Math.random() * (16 - 1)) + 1;
+  output = output + "Сгенерированное число[" + i + "из" + 10 + "]=" + randomNumbers[i] + "\n";
 }
 
 var searchNumber = prompt("Число для поиска: ");
@@ -17,14 +17,14 @@ if ((searchNumber == "") || (isNaN(searchNumber))) {
 }  
 
 for(i = 1; i <= 10; i++) {
-  if (randomNumber[i] == searchNumber) {
+  if (randomNumbers[i] == searchNumber) {
     isFound = true;
   }	
 }
 
-if (isFound == true) {
-  output = output + "Число " + searchNumber + " найдено!";
-} else {
+if (!isFound) {
   output = output + "Число " + searchNumber + " не найдено!";
+} else {
+  output = output + "Число " + searchNumber + " найдено!";
 }
 alert(output);
