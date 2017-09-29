e26d52f1b7b63e92a45a88903e36a9055f363b01
TitovVladimir <chazzii1337@gmail.com>
2017-09-29 20:42:28 +0300

    start fix branch

--- lw3/js/registration.js
+++ lw3/js/registration.js
@@ -1,27 +1,23 @@
-﻿function validate(form){
+﻿function registration(form){
   var userEmail = $("#email").val();
   var userPass = $("#password").val();
-  var userPassCheck = $("#passwordcheck").val();
-  var userAgreement = $("#checkagreement").prop("checked");
+  var userPassCheck = $("#password_repeat").val();
+  var userAgreement = $("#check_agreement").prop("checked");
   var CheckMail = (/[^\s@]+@[^\s@]+\.[^\s@]+/.test(userEmail));
 
   if ((userEmail.length < 5) || (!CheckMail)) {
     alert("Не правильно введён Email.");
-    windows.stop();
   }
 
   if (userPass.length < 6) {
     alert("Пароль не соответствует требованиям!");
-    windows.stop();
   }
 
   if (userPass != userPassCheck) {
     alert("Пароли не совпадают!");
-    windows.stop();
   }
 
   if (!userAgreement){
     alert("Соглашение не подтвержденно!");
-    windows.stop();
   }
 };
