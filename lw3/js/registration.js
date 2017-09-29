66e2119850c8f3c9a7d29d70ce1d2403189f84db
TitovVladimir <chazzii1337@gmail.com>
2017-09-29 22:26:02 +0300

    fix js branch

--- lw3/js/registration.js
+++ lw3/js/registration.js
@@ -1,25 +1,29 @@
 ﻿function registration(){
   var userEmail = $("#email").val();
   var userPass = $("#password").val();
-  var userPassCheck = $("#password_repeat").val();
+  var userPassRepeat = $("#password_repeat").val();
   var userAgreement = $("#check_agreement").prop("checked");
   var CheckMail = (/[^\s@]+@[^\s@]+\.[^\s@]+/.test(userEmail));
-
-  if ((userEmail.length < 5) || (!CheckMail)) {
-    alert("Не правильно введён Email.");
-  } else {
-    if (userPass.length < 6) {
-      alert("Пароль не соответствует требованиям!");
+  
+  if ((userEmail != "") || (userPass != "") || (userPassRepeat != "")) {
+    if ((userEmail.length < 5) || (!CheckMail)) {
+      alert("Не правильно введён Email.");
     } else {
-      if (userPass != userPassCheck) {
-        alert("Пароли не совпадают!");
+      if (userPass.length < 6) {
+        alert("Пароль не соответствует требованиям!");
       } else {
-        if (!userAgreement){
-          alert("Соглашение не подтвержденно!");
+        if (userPass != userPassCheck) {
+          alert("Пароли не совпадают!");
         } else {
-          alert("Регистрация завершенна!");
-        }
+          if (!userAgreement){
+            alert("Соглашение не подтвержденно!");
+          } else {
+            alert("Регистрация завершенна!");
+          }
+	    }
 	  }
-	}
+    }
+  } else {
+    alert("Не все поля заполнены!");
   }
 };
